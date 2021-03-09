8b3393759d3fce2c5d796479f2b8740575268462
Bettyaddis <77288923+Bettyaddis@users.noreply.github.com>
2021-03-09 11:56:05 -0600

    This is a Netflix clone project built using React

---
+++ build/precache-manifest.38058e217ca6fb2e50dacf35c0b84133.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0b5f82a52750306494738df4c5c67548",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b7706f3430305bc8c711",
+    "url": "/static/css/main.b215b06a.chunk.css"
+  },
+  {
+    "revision": "92f2502db03248df14ab",
+    "url": "/static/js/2.7199c33c.chunk.js"
+  },
+  {
+    "revision": "894f61387b383e6e2e0543fb120b4656",
+    "url": "/static/js/2.7199c33c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b7706f3430305bc8c711",
+    "url": "/static/js/main.e61bf63c.chunk.js"
+  },
+  {
+    "revision": "07b33697149324db54e5",
+    "url": "/static/js/runtime-main.b8a87127.js"
+  }
+]);
